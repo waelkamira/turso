9a201759b8245f574a241a3e41499942246dde36
waelkamira <ramondm4@gmail.com>
2024-07-23 11:40:45 +0300

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // distDir: 'build',
-
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
